853f12c92f2190c7deb1c689b836c23711c26165
HappyZifus <63356477+HappyZifus@users.noreply.github.com>
2025-08-14 13:45:46 +0300

    Update supabaseClient.js

--- supabase/supabaseClient.js
+++ supabase/supabaseClient.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mlkkehhdymhqctxlioov.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sa2tlaGhkeW1ocWN0eGxpb292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNTczMzYsImV4cCI6MjA3MDczMzMzNn0.zE3B0Awm6eS3Gw7WdCu8MlsMJf8tqIkQo4ADiEzKi1'
 const supabase = createClient(supabaseUrl, supabaseKey)
