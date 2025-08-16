d77ccde3e2dc902e104d5bcd76c37d7f2b4025cc
HappyZifus <63356477+HappyZifus@users.noreply.github.com>
2025-08-16 16:52:51 +0300

    Revert "Update main.js"
    This reverts commit 2206b148a91538f1aeb487385dd0728fbbde5fc2.

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 const SUPABASE_URL = "https://mlkkehhdymhqctxlioov.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sa2tlaGhkeW1ocWN0eGxpb292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNTczMzYsImV4cCI6MjA3MDczMzMzNn0.zE3B0Awm6eS3Gw7WdCu8MlsMJf8tqIkQo4ADiEzKi1o
-";
+const SUPABASE_KEY = "YOUR_ANON_KEY"; // замените на ваш anon key
 
 let currentPersonId = null;
 let currentYear = 2025;
